2bb56d7c61d1ee6adae230781b4d7c5f0026b9e9
Ying Huang <k6v9a@ugrad.cs.ubc.ca>
2017-11-08 17:10:33 -0800

    added visit table, changed patientId type

--- dbscripts/CreatePatientsTable.sql
+++ dbscripts/CreatePatientsTable.sql
@@ -8,12 +8,12 @@ CREATE TABLE Patient(
 	postal_code VARCHAR(32),
 	PRIMARY KEY (patientid)
 );
-insert into patient (pfirst_name, plast_name, phone_number, address, postal_code)
-VALUES('John', 'Doe', '6041211111', '1234 Main St', 'V1V1A1'); 
-insert into patient (pfirst_name, plast_name, phone_number, address, postal_code)
-VALUES('Adam', 'Armstrong', '2501009999', '222 Marine Drive', 'V1A9V9'); 
-insert into patient (pfirst_name, plast_name, phone_number, address, postal_code) 
-VALUES('Ben', 'Brown', '7780001111', '333 Canada Way', 'V2Q2R4'); 
-insert into patient (pfirst_name, plast_name, phone_number, address, postal_code) 
-VALUES('Connor', 'Clark', '6141234567', '999 Queen St', 'T5U9I0'); 
+insert into patient
+VALUES(DEFAULT,'John', 'Doe', '6041211111', '1234 Main St', 'V1V1A1'); 
+insert into patient
+VALUES(DEFAULT,'Adam', 'Armstrong', '2501009999', '222 Marine Drive', 'V1A9V9'); 
+insert into patient 
+VALUES(DEFAULT, 'Ben', 'Brown', '7780001111', '333 Canada Way', 'V2Q2R4'); 
+insert into patient 
+VALUES(DEFAULT, 'Connor', 'Clark', '6141234567', '999 Queen St', 'T5U9I0'); 
 SELECT * FROM Patient;
